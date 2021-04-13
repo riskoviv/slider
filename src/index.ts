43d1edbb2b00eec393d495379add71af9487de5c
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-04-13 15:53:49 +0500

    index.ts: changes accordingly to fix eslint's errors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import './styles/styles.scss';
 
 $(() => {
-  let $h1: JQuery<HTMLHeadingElement> = $('.heading-1');
-  $h1.html('hello Webpack & TS!');
-  
-  let $span: JQuery<HTMLSpanElement> = $('.text-block__caption');
-  $span.on('click', function (): void {
-    $(this).toggleClass('text-block__caption_highlighted');
-  })
-});
+  const $h1: JQuery<HTMLHeadingElement> = $('.heading-1');
+  $h1.html('hello TypeScript using Webpack!');
 
+  const $span: JQuery<HTMLSpanElement> = $('.text-block__caption');
+  $span.on('click', function clickOnSpan(): void {
+    $(this).toggleClass('text-block__caption_highlighted');
+  });
+});
