5915a39607a3651eeec023eec4307b0ed62c968e
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-05-13 22:36:34 +0500

    rename index to slider-plugin; begin basis

---
+++ src/slider-plugin.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import $ from 'jquery';
+import SliderModel from './model/SliderModel';
+import SliderView from './views/SliderView';
+import SliderPresenter from './presenter/SliderPresenter';
+import './styles/styles.scss';
+
+$.fn.sliderPlugin = Object.assign<ISliderPluginFunction, ISliderPluginGlobalOptions>(
+  function sliderInit (this: JQuery, options: ISliderPluginOptions): JQuery {
+    options = $.extend({}, $.fn.sliderPlugin.options, options);
+  },
+  {
+    options: {
+      pluginSelector: '',
+    },
+  },
+);
