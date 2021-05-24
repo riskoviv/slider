33ed6ef5121cccd9f136e94d28bc539fdcf6c9e9
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-05-24 22:39:06 +0500

    model: add methods for changing & returning opts

--- src/SliderModel.ts
+++ src/SliderModel.ts
@@ -3,6 +3,22 @@ import EventEmitter from './EventEmitter';
 class SliderModel extends EventEmitter {
   constructor(private options: ISliderPluginOptions) {
     super();
+    console.log(options);
+  }
+
+  getOptions(): ISliderPluginOptions {
+    return this.options;
+  }
+
+  setStepSize(stepSize: number): void {
+    this.options.stepSize = stepSize;
+    console.log(`stepSize was changed to ${stepSize}`);
+    console.log(this.getOptions());
+  }
+
+  publicMethods: Object = {
+    getOptions: this.getOptions.bind(this),
+    setStepSize: this.setStepSize.bind(this),
   }
 }
 
