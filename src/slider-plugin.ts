226084489397580e46f62095b3cb298c9a40339e
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-05-24 22:41:46 +0500

    change plugin to return public model methods

--- src/slider-plugin.ts
+++ src/slider-plugin.ts
@@ -6,14 +6,14 @@ import SliderPresenter from './SliderPresenter';
 import './styles/styles.scss';
 
 $.fn.sliderPlugin = Object.assign<ISliderPluginFunction, ISliderPluginGlobalOptions>(
-  function sliderPlugin(this: JQuery, options: ISliderPluginOptions): JQuery {
+  (options: ISliderPluginOptions): Object => {
     const pluginOptions = $.extend({}, $.fn.sliderPlugin.options, options);
     const model = new SliderModel(pluginOptions);
     const view = new SliderView();
     // eslint-disable-next-line no-unused-vars
     const presenter = new SliderPresenter(model, view);
 
-    return this;
+    return model.publicMethods;
   },
   {
     options: {},
