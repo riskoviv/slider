6aba3d1f6d6073ddce75ff7f1a70761071d00e2d
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-05-25 19:06:29 +0500

    remake presenter to inst. m&v
    add model & view instantiation;
    rootElem (this elem from plugin function) sends to view;
    plugin options sends to model;
    public methods gathering from model;
    add options logging in changeSliderColor event

--- src/SliderPresenter.ts
+++ src/SliderPresenter.ts
@@ -2,15 +2,32 @@ import SliderModel from './SliderModel';
 import SliderView from './SliderView';
 
 class SliderPresenter {
+  private model: SliderModel;
+
+  private view: SliderView;
+
+  publicMethods: Object;
+
   constructor(
-    private model: SliderModel,
-    private view: SliderView,
+    private pluginRootElem: JQuery<HTMLElement>,
+    private pluginOptions: ISliderPluginOptions,
   ) {
-    view.on('firstSliderClicked', this.changeSliderColor);
+    this.model = new SliderModel(this.pluginOptions);
+    this.view = new SliderView(this.pluginRootElem);
+
+    this.publicMethods = this.model.publicMethods;
+
+    this.view.on('viewInit', this.getOptionsFromModel);
+    this.view.on('sliderElementClicked', this.changeSliderColor);
+  }
+
+  getOptionsFromModel() {
+    this.view.pluginOptions = this.model.getOptions();
   }
 
   changeSliderColor = (target: HTMLDivElement) => {
-    SliderView.changeSliderColor(target);
+    this.view.changeSliderColor(target);
+    console.log(this.model.getOptions());
   }
 }
 
