9bb5723a9ae3e27f1b3206de47ad130f68073aa6
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-05-27 22:36:38 +0500

    add subviews instancing & appending to main view

--- src/SliderView.ts
+++ src/SliderView.ts
@@ -1,14 +1,25 @@
 import $ from 'jquery';
 import EventEmitter from './EventEmitter';
-
-// type SliderViewElements = {
-//   [key: string]: JQuery<HTMLElement>;
-// };
+import SliderBaseView from './subviews/SliderBaseView';
+import SliderHandleView from './subviews/SliderHandleView';
 
 class SliderView extends EventEmitter {
+
+  private subViews: {
+    [subViewName: string]: ISliderSubView;
   }
 
+  constructor(private pluginRootElem: JQuery<HTMLElement>) {
+    super();
+    this.subViews = {
+      sliderBase: new SliderBaseView(),
+      sliderHandle1: new SliderHandleView(),
+      sliderHandle2: new SliderHandleView(),
+    };
 
+    Object.keys(this.subViews).forEach((subView) => {
+      this.HTML.append(this.subViews[subView].HTML);
+    });
   }
 
   }
