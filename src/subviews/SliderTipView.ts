58369ac236ea9985cb58539ca1026839865359ca
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-06-10 22:11:06 +0500

    del js-slider__tip class

--- src/subviews/SliderTipView.ts
+++ src/subviews/SliderTipView.ts
@@ -1,7 +1,7 @@
 import EventEmitter from '../EventEmitter';
 
 class SliderTipView extends EventEmitter implements ISliderTipView {
-  $elem = $('<div class="slider__tip js-slider__tip"></div>');
+  $elem = $('<div class="slider__tip"></div>');
 
   setValue(value: number) {
     this.$elem.text(value);
