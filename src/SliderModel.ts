c309ff3b24f4884c93bb42dd8ce4943905bf8b94
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-06-20 22:24:30 +0500

    add allowedRealValues[] creation
    it will be used instead of translatePercentageToRealValue(): value taked by index from allowedRealValues[] will be passed in emit method value1Changed and will be used by sliderScale & sliderTip

--- src/SliderModel.ts
+++ src/SliderModel.ts
@@ -1,8 +1,11 @@
 import EventEmitter from './EventEmitter';
 
 class SliderModel extends EventEmitter implements ISliderModel {
+  allowedRealValues: number[];
+
   constructor(private options: ISliderPluginOptions) {
     super();
+    this.createAllowedRealValuesArr();
   }
 
   getOptions(): ISliderPluginOptions {
@@ -20,6 +23,29 @@ class SliderModel extends EventEmitter implements ISliderModel {
     const realRange = max - min;
     const realValue = Math.round(realRange * (percentValue / 100) + min);
     return realValue;
+  }
+
+  identifyStepSizeFractionalPrecision(): number {
+    const stepAsString = this.options.stepSize.toString();
+    if (stepAsString.indexOf('.') === -1) return 0;
+    return (stepAsString.length - 1) - stepAsString.indexOf('.');
+  }
+
+  createAllowedRealValuesArr(): void {
+    this.allowedRealValues = [];
+    const stepPrecision = this.identifyStepSizeFractionalPrecision();
+    for (
+      let stepValue = this.options.minValue;
+      stepValue <= this.options.maxValue;
+      stepValue += this.options.stepSize
+    ) {
+      this.allowedRealValues.push(Number(stepValue.toFixed(stepPrecision)));
+    }
+    const allowedRealValuesLastValue = this.allowedRealValues[this.allowedRealValues.length - 1];
+    if (allowedRealValuesLastValue < this.options.maxValue) {
+      this.allowedRealValues.push(this.options.maxValue);
+    }
+    console.log(this.allowedRealValues);
   }
 
   setValue1(handle1percent: number): void {
