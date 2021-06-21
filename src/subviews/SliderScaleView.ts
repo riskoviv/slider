7d6ffa8eb8d3ee3d5290c4b1a5695f43631d34d4
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-06-21 23:22:25 +0500

    optimizeValuesCount first version; optimization
    • divide creation of valueElements from appending to scale;
    • add optimizeValuesCount() for detection of elements overlap (early version, not working properly yet).

--- src/subviews/SliderScaleView.ts
+++ src/subviews/SliderScaleView.ts
@@ -3,21 +3,43 @@ import EventEmitter from '../EventEmitter';
 class SliderScaleView extends EventEmitter implements ISliderSubView {
   $elem = $('<div class="slider__scale"></div>');
 
+  private valueElements: JQuery<HTMLSpanElement>[];
+
   constructor(public allowedValues: number[], private allowedRealValues: number[]) {
     super();
 
-    this.allowedValues.forEach((value, index) => {
-      this.$elem.append($(`
+    this.valueElements = this.allowedValues.map((value, index) => (
+      $(`
         <span class="slider__scale-value" data-index="${index}" style="left: ${value}%">
           ${this.allowedRealValues[index]}
         </span>
-      `));
-    });
+      `)
+    ));
+
+    this.$elem.append(this.valueElements);
+
+    setTimeout(() => {
+      this.optimizeValuesCount();
+    }, 0);
+
+    // const scaleResizeObserver = new ResizeObserver((entries) => {
+    //   this.optimizeValuesCount();
+    // }).observe(this.$elem.get()[0]);
 
     this.$elem.on('click', this.scaleValueClick);
   }
 
-  scaleValueClick = (e: JQuery.ClickEvent) => {
+  private optimizeValuesCount() {
+    this.valueElements
+      .forEach(($valueEl) => {
+        if ($valueEl.next().length !== 0
+          && $valueEl.position().left + $valueEl.width() > $valueEl.next().position()?.left) {
+          $valueEl.hide();
+        }
+      });
+  }
+
+  private scaleValueClick = (e: JQuery.ClickEvent) => {
     if (e.target.classList.contains('slider__scale-value')) {
       this.emit('scaleValueSelect', this.allowedValues[e.target.dataset.index]);
     }
