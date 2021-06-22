42200ca40774232777822cc3828d2df189fbc4ed
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-06-22 22:53:32 +0500

    optimizeValuesCount() development

--- src/subviews/SliderScaleView.ts
+++ src/subviews/SliderScaleView.ts
@@ -20,23 +20,57 @@ class SliderScaleView extends EventEmitter implements ISliderSubView {
 
     setTimeout(() => {
       this.optimizeValuesCount();
-    }, 0);
+    }, 1000);
+
+    setTimeout(() => {
+      this.optimizeValuesCount();
+    }, 2000);
 
     // const scaleResizeObserver = new ResizeObserver((entries) => {
-    //   this.optimizeValuesCount();
+    //   setTimeout(() => {
+    //     this.optimizeValuesCount();
+    //   }, 2000);
     // }).observe(this.$elem.get()[0]);
 
     this.$elem.on('click', this.scaleValueClick);
   }
 
   private optimizeValuesCount() {
+    // while (this.hasOverlappingValues()) {
+    let $lastOversteppedEl: JQuery<HTMLSpanElement>;
+
+    this.valueElements.slice(1).forEach(($valueEl, i, valueElements) => {
+      const currentElRightBound = $valueEl.position().left + $valueEl.width();
+      const $nextEl = valueElements[i + 1]; // find next el w/o 'hidden' here
+      const isLastEl = i === valueElements.length - 1;
+      const hasNext = $nextEl !== undefined && !$nextEl.data('hidden');
+      const nextElPosition = hasNext ? $nextEl.position().left : null;
+      const notOversteppedAndHasNext = ($valueEl !== $lastOversteppedEl && hasNext);
+      const overlapsNext = nextElPosition > 0 && currentElRightBound >= nextElPosition;
+
+      if (notOversteppedAndHasNext && overlapsNext) {
+        console.log(currentElRightBound, nextElPosition);
+        $lastOversteppedEl = $nextEl;
+        $valueEl.hide().data('hidden', 'true');
+      } /* else if (!overlapsNext) {
+        $valueEl.show().data('hidden', 'false');
+      } */
+    });
+    // }
+  }
+
+  private hasOverlappingValues(): boolean {
     this.valueElements
-      .forEach(($valueEl) => {
-        if ($valueEl.next().length !== 0
-          && $valueEl.position().left + $valueEl.width() > $valueEl.next().position()?.left) {
-          $valueEl.hide();
+      .forEach(($valueEl, i) => {
+        if (
+          this.valueElements[i + 1] !== undefined
+          && ($valueEl.position().left + $valueEl.width()) >= this.valueElements[i + 1].position()?.left
+        ) {
+          return true;
         }
+        return false;
       });
+    return false;
   }
 
   private scaleValueClick = (e: JQuery.ClickEvent) => {
