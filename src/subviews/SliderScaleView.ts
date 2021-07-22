1ff2e3fbb396dd3d69bae2e6d0a915fc238b7095
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-07-22 22:30:41 +0500

    add scale values count optimization

--- src/subviews/SliderScaleView.ts
+++ src/subviews/SliderScaleView.ts
@@ -5,44 +5,61 @@ class SliderScaleView extends EventEmitter implements ISliderSubView {
 
   private valueElements: JQuery<HTMLSpanElement>[];
 
-  constructor(public allowedValues: number[], private allowedRealValues: number[]) {
+  constructor(public allowedPositions: number[], private allowedRealValues: number[]) {
     super();
 
-    this.valueElements = this.allowedValues.map((value, index) => (
-      $(`
-        <div class="slider__scale-block" data-index="${index}" style="left: ${value}%">
-          <span class="slider__scale-text">${this.allowedRealValues[index]}</span>
-        </div>
-      `)
-    ));
-
-    this.$elem.append(this.valueElements);
-
     setTimeout(() => {
+      this.createValuesElements();
+      this.$elem.append(this.valueElements);
       this.optimizeValuesCount();
+      new ResizeObserver(() => {
+        this.optimizeValuesCount();
+      }).observe(this.$elem.get()[0]);
     }, 0);
-
-    new ResizeObserver(() => {
-      this.optimizeValuesCount();
-    }).observe(this.$elem.get()[0]);
 
     this.$elem.on('click', this.scaleValueClick);
   }
 
+  private createValuesElements = () => {
+    const quotient = Math.round((this.allowedPositions.length / this.$elem.width()) * 3);
+    console.log('quotient: ', `${quotient} = ${this.allowedPositions.length} / ${this.$elem.width()} * 3`);
+
+    const everyValueIsAllowed = quotient === 0;
+
+    this.valueElements = this.allowedPositions.map((value, index, array) => {
+      const isEdgeValue = index === 0 || index === array.length - 1;
+      const isMultiple = index % quotient === 0;
+      // eslint-disable-next-line fsd/split-conditionals
+      if (everyValueIsAllowed || isEdgeValue || isMultiple) {
+        return $(`
+          <div class="slider__scale-block" data-index="${index}" style="left: ${value}%">
+            <span class="slider__scale-text">${this.allowedRealValues[index]}</span>
+          </div>
+        `);
+      }
+
+      return null;
+    });
+  }
+
   private optimizeValuesCount() {
+    const $firstElem = this.valueElements[0];
     const $lastElem = this.valueElements[this.valueElements.length - 1];
-    let $currentElem = this.valueElements[0];
+    let $currentElem = $firstElem;
+
     this.valueElements.slice(1).forEach(($elem) => {
       const curElemRightBound = $currentElem.position().left + $currentElem.width();
-      if ($elem.position().left - 5 <= curElemRightBound) {
-        if ($elem === $lastElem && $currentElem !== this.valueElements[0]) {
-          $currentElem.addClass('slider__scale-block_invisible');
-        } else if ($elem !== $lastElem) {
-          $elem.addClass('slider__scale-block_invisible');
+      if ($elem) {
+        if ($elem.position().left - 5 <= curElemRightBound) {
+          if ($elem === $lastElem && $currentElem !== $firstElem) {
+            $currentElem.addClass('slider__scale-block_unnumbered');
+          } else if ($elem !== $lastElem) {
+            $elem.addClass('slider__scale-block_unnumbered');
+          }
+        } else {
+          $currentElem = $elem;
+          $elem.removeClass('slider__scale-block_unnumbered');
         }
-      } else {
-        $currentElem = $elem;
-        $elem.removeClass('slider__scale-block_invisible');
       }
     });
   }
@@ -50,7 +67,7 @@ class SliderScaleView extends EventEmitter implements ISliderSubView {
   private scaleValueClick = (e: JQuery.ClickEvent) => {
     const target = e.target.closest('.slider__scale-block');
     if (target) {
-      this.emit('scaleValueSelect', this.allowedValues[target.dataset.index]);
+      this.emit('scaleValueSelect', this.allowedPositions[target.dataset.index]);
     }
   }
 }
