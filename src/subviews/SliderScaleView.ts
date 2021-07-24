2e6ce121c70224e8bc5b2a8622d761dbb5ae8cdb
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-07-24 23:34:18 +0500

    scale values elements' new count optimization

--- src/subviews/SliderScaleView.ts
+++ src/subviews/SliderScaleView.ts
@@ -23,24 +23,37 @@ class SliderScaleView extends EventEmitter implements ISliderSubView {
   private createValuesElements = () => {
     const quotient = Math.round((this.allowedPositions.length / this.$elem.width()) * 3);
     console.log('quotient: ', `${quotient} = ${this.allowedPositions.length} / ${this.$elem.width()} * 3`);
+    const lastElemIndex = this.allowedPositions.length - 1;
 
-    const everyValueIsAllowed = quotient === 0;
+    this.valueElements = [];
 
-    this.valueElements = this.allowedPositions.map((value, index, array) => {
-      const isEdgeValue = index === 0 || index === array.length - 1;
-      const isMultiple = index % quotient === 0;
-      // eslint-disable-next-line fsd/split-conditionals
-      if (everyValueIsAllowed || isEdgeValue || isMultiple) {
-        return $(`
-          <div class="slider__scale-block" data-index="${index}" style="left: ${value}%">
-            <span class="slider__scale-text">${this.allowedRealValues[index]}</span>
-          </div>
-        `);
+    const isEveryValueAllowed = [0, 1].includes(quotient);
+
+    if (isEveryValueAllowed) {
+      this.valueElements = this.allowedPositions.map((value, index) => (
+        this.makeNewScaleValueElement(index, value)
+      ));
+    } else {
+      for (let index = 0; index <= lastElemIndex; index += quotient) {
+        this.valueElements.push(this.makeNewScaleValueElement(index, this.allowedPositions[index]));
       }
 
-      return null;
-    });
+      const isLastElemIsNotMaxValue = this.valueElements[this.valueElements.length - 1].data('index') !== lastElemIndex;
+      if (isLastElemIsNotMaxValue) {
+        this.valueElements.push(
+          this.makeNewScaleValueElement(lastElemIndex, this.allowedPositions[lastElemIndex]),
+        );
+      }
+    }
   }
+
+  private makeNewScaleValueElement = (index: number, value: number) => (
+    $(`
+      <div class="slider__scale-block" data-index="${index}" style="left: ${value}%">
+        <span class="slider__scale-text">${this.allowedRealValues[index]}</span>
+      </div>
+    `)
+  );
 
   private optimizeValuesCount() {
     const $firstElem = this.valueElements[0];
