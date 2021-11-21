f7f82aed1b4de3ff1dbe86936c79e8faf8b80e67
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-11-21 23:00:11 +0500

    add custom options

--- src/demo/demo-page.js
+++ src/demo/demo-page.js
@@ -2,5 +2,16 @@ import './demo-page-styles.scss';
 
 $(() => {
   const $slider1 = $('#slider');
-  const slider1 = $slider1.sliderPlugin();
+  const slider1 = $slider1.sliderPlugin({
+    // stepSize: 1,
+    // minValue: -250,
+    // maxValue: 250,
+    value1: -104,
+    value2: -102,
+    // isVertical: false,
+    // isInterval: true,
+    // showTip: true,
+    // showScale: true,
+    // showProgressBar: true,
+  });
 });
