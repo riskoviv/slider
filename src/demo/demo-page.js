72d25d037d0753b62119d4625a7943ba5d9b3543
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-11-23 19:27:33 +0500

    experimantal code changes

--- src/demo/demo-page.js
+++ src/demo/demo-page.js
@@ -3,15 +3,17 @@ import './demo-page-styles.scss';
 $(() => {
   const $slider1 = $('#slider');
   const slider1 = $slider1.sliderPlugin({
-    // stepSize: 1,
+    stepSize: 3,
     // minValue: -250,
     // maxValue: 250,
-    value1: -104,
-    value2: -102,
-    // isVertical: false,
-    // isInterval: true,
+    // value1: -104,
+    // value2: -102,
+    // isVertical: true,
+    // isInterval: false,
     // showTip: true,
     // showScale: true,
     // showProgressBar: true,
   });
+
+  // console.log('slider1.getOptions(): ', slider1.getOptions());
 });
