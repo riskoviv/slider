0eefb42437653a8e2e33cf5b6444652ed48e5c1b
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2022-01-13 22:23:17 +0500

    plugin init tests

--- src/demo/demo-page.js
+++ src/demo/demo-page.js
@@ -2,12 +2,12 @@ import './demo-page-styles.scss';
 
 $(() => {
   const $slider1 = $('#slider');
-  const slider1 = $slider1.sliderPlugin({
-    stepSize: 15,
-    // minValue: -250,
-    // maxValue: 250,
-    // value1: -104,
-    // value2: -102,
+  window.slider1 = $slider1.sliderPlugin({
+    stepSize: 1,
+    minValue: -250,
+    maxValue: -150,
+    value1: -204,
+    value2: -170,
     // isVertical: true,
     // isInterval: false,
     // showTip: true,
@@ -15,5 +15,5 @@ $(() => {
     // showProgressBar: true,
   });
 
-  // console.log('slider1.getOptions(): ', slider1.getOptions());
+  console.log('slider1.getOptions(): ', window.slider1.getOptions());
 });
