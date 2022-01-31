6b09ac51b6780f109fbf2b5c9f25ddcbf1284ca5
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2022-01-31 16:20:16 +0500

    change demp page test js code

--- src/demo/demo-page.js
+++ src/demo/demo-page.js
@@ -3,11 +3,11 @@ import './demo-page-styles.scss';
 $(() => {
   const $slider1 = $('#slider');
   window.slider1 = $slider1.sliderPlugin({
-    stepSize: 1,
-    minValue: -250,
-    maxValue: -150,
-    value1: -204,
-    value2: -170,
+    stepSize: 3,
+    minValue: 0,
+    maxValue: 35,
+    value1: 10,
+    value2: 31,
     // isVertical: true,
     // isInterval: false,
     // showTip: true,
@@ -15,5 +15,5 @@ $(() => {
     // showProgressBar: true,
   });
 
-  console.log('slider1.getOptions(): ', window.slider1.getOptions());
+  console.log('slider1.getOptions(): ', window.slider1.debug.getOptions());
 });
