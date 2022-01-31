c74d0f7d47797973dce48f80342654ce017271e8
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2022-01-31 13:40:36 +0500

    fix mistake in creating of elements on scale
    it only accounted width of scale element and properly worked only in horizontal mode, now if slider in vertical mode it takes height of scale element and works in vertical mode properly now

--- src/subviews/SliderScaleView.ts
+++ src/subviews/SliderScaleView.ts
@@ -31,7 +31,7 @@ class SliderScaleView extends EventEmitter {
   }
 
   private createValuesElements = () => {
-    const scaleSize = this.$elem.width() || 1;
+    const scaleSize = this.$elem[this.dimension]() || 1;
     const quotient = Math.round((this.allowedPositions.length / scaleSize) * 3);
     const lastElemIndex = this.allowedPositions.length - 1;
     const isEveryValueAllowed = [0, 1].includes(quotient);
