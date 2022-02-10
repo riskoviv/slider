bb0aed15d737128c83f1cb7283b9b2f4a07dcb0c
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2022-02-10 22:47:49 +0500

    change SliderView to match View class
    also implement its own render() & other methods;
    create type SliderViewOptions

--- src/subviews/SliderView.ts
+++ src/subviews/SliderView.ts
@@ -1,13 +1,31 @@
 import View from '../View';
 
-class SliderView extends View {
-  readonly $elem: JQuery<HTMLDivElement> = $('<div class="slider"></div>');
+type SliderViewOptions = {
+  isVertical: boolean,
+  isInterval: boolean,
+};
 
+class SliderView extends View {
   readonly $controlContainer: JQuery<HTMLDivElement> = $('<div class="slider__control-container"></div>');
 
-  protected render = (): JQuery<HTMLDivElement> => (
-    $('<div class="slider"></div>')
-  );
+  constructor(options: SliderViewOptions) {
+    super();
+    this.render(options);
+  }
+
+  protected render(
+    options: SliderViewOptions = { isVertical: false, isInterval: false },
+  ): JQuery<HTMLElement> {
+    return $(`<div class="slider${options.isVertical ? ' slider_vertical' : ''}${options.isInterval ? ' slider_interval' : ''}"></div>`).append(this.$controlContainer);
+  }
+
+  toggleVertical(): void {
+    this.$elem.toggleClass('slider_vertical');
+  }
+
+  toggleInterval(): void {
+    this.$elem.toggleClass('slider_interval');
+  }
 
   setPosition(position: number): void {
     this.$elem.css(`--${this.viewType}-position`, `${position}%`);
