78fc621484608cbfd978863a110fb7e25dd04932
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2022-02-16 22:06:14 +0500

    SubView: rm constructor; change render();

--- src/SubView.ts
+++ src/SubView.ts
@@ -1,19 +1,15 @@
 import EventEmitter from './EventEmitter';
 
 abstract class SubView extends EventEmitter implements ISubView {
-  readonly $elem: JQuery<HTMLElement>;
+  $elem: JQuery<HTMLElement> = $('<div></div>');
 
   protected abstract readonly viewType: ViewType;
 
   protected readonly elementNumber?: 1 | 2;
 
-  constructor() {
-    super();
-    this.$elem = this.render();
-  }
-
-  protected render(): JQuery<HTMLElement> {
-    return $(`<div class="slider__${this.viewType}${this.elementNumber !== undefined ? ` slider__${this.viewType}_${this.elementNumber}` : ''}"></div>`);
+  render(): JQuery<HTMLElement> {
+    this.$elem = $(`<div class="slider__${this.viewType}${this.elementNumber !== undefined ? ` slider__${this.viewType}_${this.elementNumber}` : ''}"></div>`);
+    return this.$elem;
   }
 
   removeView(): void {
