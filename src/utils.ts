241181502c8efcfaefe7131eccbc6cd29ced6f19
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2022-02-25 22:09:48 +0500

    add getTypedKeys() to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,15 @@
 const utils = {
   /**
-   * use Object.entries method but return entries of passed object
+   * uses Object.keys method but returns array of keys
+   * of passed object in original type that keys of object has
+   * @param obj object to get keys from
+   * @returns array of object's keys but of original type
+   */
+  getTypedKeys<T extends Record<string, unknown>>(obj: T): (keyof T)[] {
+    return Object.keys(obj);
+  },
+  /**
+   * uses Object.entries method but returns entries of passed object
    * with keys in original type
    * (not in string type as it does regular Object.entries method)
    * @param obj object to get entries from
