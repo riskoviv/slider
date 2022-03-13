6332aaeeff5718f1cdff90457182d5800e50236a
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2022-03-13 13:54:16 +0500

    move setThumbThickness to View, use it on init

--- src/subviews/ThumbView.ts
+++ src/subviews/ThumbView.ts
@@ -4,13 +4,6 @@ class ThumbView extends SubView implements IThumbView {
   constructor(protected readonly elementNumber: 1 | 2 = 1) {
     super(elementNumber);
   }
-
-  setThumbThickness(thickness: number): void {
-    this.$elem.css(
-      '--thumb-thickness',
-      `${thickness}%`,
-    );
-  }
 }
 
 export default ThumbView;
