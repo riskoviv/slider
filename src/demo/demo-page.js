5d8e94481d0f79aae4ec458643dfec697b973f2e
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2022-03-23 22:28:59 +0500

    test panel integration tests

--- src/demo/demo-page.js
+++ src/demo/demo-page.js
@@ -3,11 +3,11 @@ import './demo-page-styles.scss';
 $(() => {
   const $slider1 = $('#slider1');
   window.slider1 = $slider1.sliderPlugin({
-    stepSize: 3,
-    minValue: 0,
-    maxValue: 175,
-    value1: 10,
-    value2: 31,
+    stepSize: 10_000,
+    minValue: 120_000,
+    maxValue: 1_200_000,
+    value1: 100_000,
+    value2: 310_000,
     // isVertical: true,
     isInterval: true,
     showTip: true,
@@ -20,6 +20,31 @@ $(() => {
   //   showScale: true,
   // });
 
-  console.log('slider1.getOptions(): ', window.slider1.debug.getOptions());
-  console.log('slider2.getOptions(): ', window.slider2.debug.getOptions());
+  // console.log('slider2.getOptions(): ', window.slider2.debug.getOptions());
+
+  const $panel1 = $('#panel1');
+  const $panel1Interval = $('.panel__interval', $panel1);
+  const $panel1Vertical = $('.panel__vertical', $panel1);
+
+  const slider1Options = window.slider1.debug.getOptions();
+  console.log('slider1.getOptions(): ', slider1Options);
+
+  if (slider1Options.isInterval) {
+    $panel1Interval.attr('checked', true);
+  }
+
+  if (slider1Options.isVertical) {
+    $panel1Vertical.attr('checked', true);
+  }
+
+  /**
+   * @this HTMLElement isInterval checkbox element
+   */
+  function slider1ChangeIsInterval() {
+    const isInterval = this.value;
+    console.log('isInterval: ', isInterval);
+    window.slider1.setInterval(isInterval);
+  }
+
+  $panel1Interval.on('change', slider1ChangeIsInterval);
 });
