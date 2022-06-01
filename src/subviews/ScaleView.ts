06ec664bbf33c00ffbf09f2677df590a3546809c
Ivan Riskov <44531490+riskoviv@users.noreply.github.com>
2022-06-01 20:13:10 +0500

    ScaleView: select value only if clicked by LMB
    event.button === 0 means left mouse button was used to raise an event
    (if a mouse was used)

--- src/subviews/ScaleView.ts
+++ src/subviews/ScaleView.ts
@@ -49,7 +49,7 @@ class ScaleView extends SubView implements IScaleView {
 
   private scaleValueClick = (e: PointerEvent): void => {
     const { target } = e;
-    if (target instanceof HTMLSpanElement) {
+    if (target instanceof HTMLSpanElement && e.button === 0) {
       this.emit('scaleValueSelect', Number(target.innerText));
     }
   }
