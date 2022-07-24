73b2c0fe073ac47a5b5c0d07c39476d633ea36be
Ivan Riskov <44531490+riskoviv@users.noreply.github.com>
2022-07-24 13:35:05 +0500

    change ResizeObserver mock implementation
    to constructor function

--- src/__tests__/mocks/ResizeObserver.ts
+++ src/__tests__/mocks/ResizeObserver.ts
@@ -1,11 +1,10 @@
+function ResizeObserverMock(this: ResizeObserver) {
+  this.observe = jest.fn();
+  this.unobserve = jest.fn();
+  this.disconnect = jest.fn();
+}
+
 Object.defineProperty(window, 'ResizeObserver', {
   writable: true,
-  value: jest.fn().mockImplementation(() => {
-    const resizeObserverInstance: ResizeObserver = {
-      observe: jest.fn(),
-      unobserve: jest.fn(),
-      disconnect: jest.fn(),
-    };
-    return resizeObserverInstance;
-  }),
+  value: jest.fn().mockImplementation(ResizeObserverMock),
 });
